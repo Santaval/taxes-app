87de71c7582e36e10255e5caf5c25ada10fd86f9
Aaron Santana <santanavaldelomaraa@gmail.com>
2025-08-16 15:05:45 -0600

    mono repo

--- server/src/utils/otp/otpGenerator.ts
+++
@@ -1,8 +0,0 @@
-export default function generateOTP() {
-  let final = "";
-  for (let count = 0; count < 4; count++) {
-    const num = parseInt(Math.random() * 10 + "");
-    final += num.toString();
-  }
-  return final;
-}
