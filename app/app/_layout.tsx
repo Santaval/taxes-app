013d458da8ed096859e03ca40978455056765ad2
Aaron Santana <santanavaldelomaraa@gmail.com>
2025-08-16 14:11:49 -0600

    feat: integrate Sentry for error tracking and monitoring

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -13,8 +13,25 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import { TransactionsProvider } from '@/contexts/TransactionsContext';
 import { DateRangeProvider } from '@/contexts/DateRangeContext';
 import { ClientsProvider } from '@/contexts/ClientsContext';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://8ece74c379f0fd5503eb9052aa3f0f67@o4507159519887360.ingest.us.sentry.io/4509851370848256',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -39,4 +56,4 @@ export default function RootLayout() {
       </AuthProvider>
     </ThemeProvider>
   );
-}
+});
